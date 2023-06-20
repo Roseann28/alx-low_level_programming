3e0ba210bfabda286f329f08ba13e84438ccf1dd
roseann28 <roseann2044@gmail.com>
2023-06-19 19:10:14 -0700

    First Assignment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
 #include <stdio.h>
+/**
+ *main - prints"Programming is like building a multilingual puzzle"
+ * Return:0
+*/
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle");
+	return (0);
+}
